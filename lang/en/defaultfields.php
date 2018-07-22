4cf8287f74a4bcf0df75b64ed45cb0f1e8b50e8b
November7 <m.kowalski@zsme.tarnow.pl>
2018-07-22 23:37:34 +0200

    settings page

---
+++ lang/en/defaultfields.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Default fields';
+$string['option'] = 'Option';
+$string['option_desc'] = 'Opis...';
+$string['xxx'] = 'opcja checkbox';
+$string['xxx_desc'] = 'Opis...';
---
+++ settings.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+
+
+    $options = [
+        'option1' => 'opcja 1',
+        'option2' => 'opcja 2',
+        'option3' => 'opcja 3'];
+
+    $setting = new admin_setting_configselect('defaultfields/option',
+                                         new lang_string('option', 'defaultfields'),
+                                         new lang_string('option_desc', 'defaultfields'),
+                                         'opcja 1',
+                                         $options);
+    $settings->add($setting);
+
+    $setting = new admin_setting_configcheckbox('defaultfields/xxx',
+                                        new lang_string('xxx', 'defaultfields'),
+                                        new lang_string('xxx_desc', 'defaultfields'),
+                                        0);
+    $settings->add($setting);
+}
