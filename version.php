8804ff4a0f8b755fa6ad649f444b307374f7b435
Nov7 <m.kowalski@zsme.tarnow.pl>
2018-07-22 17:07:09 +0200

    Init

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2018072200; // Plugin version.
+$plugin->requires = 2018051700; // Moodle version.
+$plugin->component = 'local_defaultfields'; // Full name of the plugin (used for diagnostics).
+
+?>
