d901149d99bbb1e774fc7efbe443e06c73a87349
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2022-11-26 17:14:06 +0100

    Add python parser

--- tinymce_codehl/version.php
+++ tinymce_codehl/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2022091901;
+$plugin->version   = 2022112601;
 // Required Moodle version.
 $plugin->requires  = 2014111000;
 // Full name of the plugin (used for diagnostics).
